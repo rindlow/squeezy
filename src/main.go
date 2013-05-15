a689278e78d25cd2bcc9fc8a28a879a166fb35f9
Linus Widstromer <linus@mumindalen.net>
2013-05-15 18:59:19 +0200

    Added command line switches for the various functions

--- src/main.go
+++ src/main.go
@@ -2,38 +2,62 @@ package main
 
 import (
 	"fmt"
-	//"log"
-	//"slimserver"
+	"slimserver"
 	"musiclibrary"
 	"time"
+	"flag"
 )
 
 func main() {
-	//slimcommands := make(chan slimserver.SlimCommand)
-	//slimsrv := new(slimserver.SlimServer)
 
-	// Create the library
-	//library := new(musiclibrary.MusicLibrary)
+	var startSlim bool
+	var startDisc bool
+	var updateLibrary bool
+	var printLibrary bool
 
-	// Initiate a library update
-        fmt.Println("Updating media library")
-	c := make(chan int)
-	go func() {
-		musiclibrary.UpdateLibrary("/data/music")
-		c <- 1
-	}()
+	// Parse command line flags
+	flag.BoolVar(&startSlim, "slim", false, "Start the slimserver")
+	flag.BoolVar(&startDisc, "disco", false, "Start the discovery server")
+	flag.BoolVar(&updateLibrary, "update", true, "Initiate a library update")
+	flag.BoolVar(&printLibrary, "print", true, "Print library content on startup")
+	flag.Parse()
 
-	//go slimserver.DiscoveryServer()
-	//go slimsrv.Serve(slimcommands)
+	if(startSlim) {
+		fmt.Println("Starting slimserver");
+		slimcommands := make(chan slimserver.SlimCommand)
+		slimsrv := new(slimserver.SlimServer)
+		go slimsrv.Serve(slimcommands)
+	}
 
-	// Wait for the update to complete before continuing
-	<-c
+	if(startDisc) {
+		go slimserver.DiscoveryServer()
+	}
 
-        fmt.Println("Done with update")
+	if(updateLibrary) {
+		fmt.Println("Updating media library")
+		c := make(chan int)
+		go func() {
+			musiclibrary.UpdateLibrary("/data/music/Various")
+			c <- 1
+		}()
 
-	// Print library statistics
-        musiclibrary.PrintLibrary()
+		// Wait for the update to complete before continuing
+		<-c
+		fmt.Println("Done with update")
+	}
+
+
+	if(printLibrary) {
+		musiclibrary.PrintLibrary()
+	}
 
 	// Do stuff...
-	time.Sleep(10 * time.Second)
+	fmt.Println("Sleeping for a while...")
+	for i := 0; i < 10; i++ {
+		fmt.Printf(".");
+		time.Sleep(time.Second)
+	}
+	fmt.Printf("\n");
+
+	fmt.Println("Exiting...");
 }
