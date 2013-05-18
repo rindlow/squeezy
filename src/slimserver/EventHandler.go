020f8973b3952ac7f8a31e8668c15be0c899b9a9
Linus Widstromer <linus@mumindalen.net>
2013-05-18 08:01:43 +0200

    Added stub for an eventHandler

---
+++ src/slimserver/EventHandler.go
@@ -0,0 +1,14 @@
+package slimserver
+
+import (
+	//"fmt"
+	//"log"
+)
+
+
+// The EvenHandler should be started with a master-chan to the StreamServer
+// and the client-Server. On these master channels the instance-channels
+// should be passed. These instance channels are then uses for passing
+// events and messages.
+
+// Keep a state-machine instance for each known device 
